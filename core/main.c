b7d34a95c9f8110dcb1fac1fbad953e52de8da61
Alex Winter <eterno.despierto@gmail.com>
2014-02-10 18:29:21 -0500

    Clean up staff at main.c Add SPI to the project. Enable config of SPI.

--- core/main.c
+++ core/main.c
@@ -23,6 +23,9 @@
  *************************************************/
 #include "global.h"
 #include "drv_eth.h"
+#include "clkctrl.h"
+#include "pinmux.h"
+#include "spi.h"
 
 #define XBOOT_VERSION_R      0
 #define XBOOT_VERSION_RC     2
@@ -60,89 +63,21 @@ void  _start(void)
     return;
 }
 
-/*====================================================
- *            NET
- *====================================================*/
-//definitions
-#define CONFIG_BOOTFILE         "zlinux"	                    /* Boot file name */
-#define CONFIG_SYS_PROMPT       "x-boot> "
-#define CONFIG_BOOTOFFSET       (0x8000)
-#define CONFIG_SYS_LOAD_ADDR    (SDRAM_BASE + CONFIG_BOOTOFFSET)
-#define CONFIG_NETMASK          255.255.255.0
-#define CONFIG_IPADDR           192.168.0.200                   /* 10.1.184.112 192.168.0.136 */
-#define CONFIG_SERVERIP         192.168.0.105                   /* 10.1.184.188 192.168.0.2 */
-#define CONFIG_BOOTDELAY        3                               /* sec */
-#define PKTSIZE                 1518
-#define PKTSIZE_ALIGN           1536
-#define PKTALIGN                32
-#define PKTBUFSRX               4                               /* Rx MAX supported by ks8851 is 12KB */
-#define PKTBUFSTX               1                               /* Tx MAX supported by ks8851 is 6KB */
-
-//declarations
-char            BootFile[128];
-unsigned int    linux_load_addr;
-
-uchar          *NetArpWaitPacketMAC;                            /* MAC address of waiting packet's destination */
-uchar          *NetArpWaitTxPacket;                             /* THE transmit packet */
-int             NetArpWaitTxPacketSize;
-uchar           NetArpWaitPacketBuf[PKTSIZE_ALIGN + PKTALIGN];
-unsigned int    NetArpWaitTimerStart;
-int             NetArpWaitTry;
-IPaddr_t        NetArpWaitPacketIP;
-IPaddr_t        NetArpWaitReplyIP;
-volatile uchar  PktBuf[(PKTBUFSRX+PKTBUFSTX) * PKTSIZE_ALIGN + PKTALIGN];   
-                                                                /* 7712bytes: 1 Tx packet + 4 Rx packets */
-volatile uchar *NetTxPackets[PKTBUFSTX];                        /* Transmit packets */
-volatile uchar *NetRxPackets[PKTBUFSRX];                        /* Receive packets */
-
-unsigned int	gStatusEthernet = 0;                            /* disabled */
-
-
 static void initialization(void)
 {
-    unsigned int i;
- 
     print_log("%s", "Environment initialization");
 
+    /* Configure CPU and SSP clocks*/
+    init_clocks();
    
-    //net driver initialization
-    print_eth("%s", "Net environment initialization");
-    linux_load_addr = CONFIG_SYS_LOAD_ADDR;
-    NetArpWaitPacketMAC = NULL;
-    NetArpWaitPacketIP = 0;
-    NetArpWaitReplyIP = 0;
-    copy_filename(BootFile, CONFIG_BOOTFILE, sizeof(BootFile));
+    /* Configure SSP1 pins*/
+    init_pinmux();
 
-    //setup packet buffers, aligned correctly
-    NetTxPackets[0] = (volatile uchar *)((unsigned int)&PktBuf[0] + (PKTALIGN - 1));
-    NetTxPackets[0] = (volatile uchar *)((unsigned int)NetTxPackets[0] - (unsigned int)NetTxPackets % PKTALIGN);
-    for (i = 1; i < PKTBUFSTX; i++) {
-        NetTxPackets[i] = (volatile uchar *)((unsigned int)NetTxPackets[0] + i*PKTSIZE_ALIGN);
-    }
-    for (i = 0; i < PKTBUFSRX; i++) {
-        NetRxPackets[i] = (volatile uchar *)((unsigned int)NetTxPackets[0] + (i+PKTBUFSTX)*PKTSIZE_ALIGN);
-    }
+    /* Configure SPI on SSP1*/
+    spi_init();
 
-    print_eth(" - Net Tx packet: base_0x%x count_%d", (unsigned int)NetTxPackets[0], PKTBUFSTX);
-    print_eth(" - Net Rx packet: base_0x%x count_%d", (unsigned int)NetRxPackets[0], PKTBUFSRX);    
-    
-    NetArpWaitTxPacket = (uchar *)((unsigned int)&NetArpWaitPacketBuf[0] + (PKTALIGN - 1));
-    NetArpWaitTxPacket = (uchar *)((unsigned int)NetArpWaitTxPacket - (unsigned int)NetArpWaitTxPacket % PKTALIGN);
-    NetArpWaitTxPacketSize = 0;
-
-    print_eth(" - Net Arp Tx packet: base_0x%x count_%d", (unsigned int)NetArpWaitTxPacket, 1);
-
-    print_eth("%s", "Halt Ethernel driver whatever its condition...");
-    drv_eth_halt();                                             /* do call of common interface function */
-
-    if (drv_eth_init()) {                                    /* do call of common interface function */
-        drv_eth_halt();                                         /* do call of common interface function */
-        gStatusEthernet = 0;
-        print_err("%s", "ethernet initialization wasn't completed");
-    } else {
-        print_eth("%s", "Ethernel was successfully started");
-        gStatusEthernet = 1;
-    }
+    /* Configure Ethernt device*/
+    drv_eth_init();
 
     return;
 }
